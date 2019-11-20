d3733905fdc9eca037df00e8127ea0916e38b2cb
ohld <>
2019-11-20 23:51:01 +0300

    parse ALL (not tested in case of BAN)

---
+++ spreadthesign_parser/spreadthesign_parser/README.md
@@ -0,0 +1,9 @@
+# How to run
+
+``` bash
+scrapy crawl words -o spreadthesign.json
+```
+
+----
+
+[*@ohld*](https://okhlopkov.com) used [this](https://towardsdatascience.com/a-minimalist-end-to-end-scrapy-tutorial-part-i-11e350bcdec0) to code it.
--- spreadthesign_parser/spreadthesign_parser/spiders/words_spider.py
+++ spreadthesign_parser/spreadthesign_parser/spiders/words_spider.py
@@ -1,14 +1,22 @@
 import scrapy
 
+BESE_URL = "https://www.spreadthesign.com"
+
 class WordsSpider(scrapy.Spider):
     name = "words"
+    allowed_domains = ["spreadthesign.com"]
+    max_id = 16000  # max id I found 
 
-    start_urls = ["https://www.spreadthesign.com/ru.ru/word/1"]
+    def start_requests(self):
+            for i in range(self.max_id):
+                yield scrapy.Request(
+                    'https://www.spreadthesign.com/ru.ru/word/{0}'.format(i),
+                    callback=self.parse
+                )
+                # time.sleep
     
     def parse(self, response):
-        # self.logger.info("")
-
-        word = response.css("h2::text").get().strip()
+        word = response.css("h2::text").getall()[1].strip()
 
         synonyms = response.css(".search-result")
         synonyms_data = [{
@@ -17,13 +25,17 @@ class WordsSpider(scrapy.Spider):
             "type": s.css("small::text").get().strip(),   
         } for s in synonyms if word != s.css("a::text").get().strip()]
 
-        variants = []
-        variants_data = []
+        # TODO: some smarter stuff that ignore duplicates (or maybe scrapy will ignore pages automatically)
+        variant_urls = response.css(".search-result-content").css(".nav-tabs").css("a").xpath("@href").getall()[1:]
+        for url in variant_urls:
+            yield scrapy.Request(
+                BESE_URL + url,
+                callback=self.parse
+            )
 
         yield {
             "word": word,
             "video_link": response.css("video").xpath('@src').get(),
             "url": response.url,
             "synonyms": synonyms_data,
-            "variants": variants_data
         }
