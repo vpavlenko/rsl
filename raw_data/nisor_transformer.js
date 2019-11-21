77e2c545218fa28c00f058d83248e4386816ce5c
Vitaly Pavlenko <cxielamiko@gmail.com>
2019-11-21 03:06:09 +0300

    Add nisor_transformer

---
+++ raw_data/nisor_transformer.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+const input = require('./nisor.json');
+
+output = [];
+
+input.forEach(({video, word}) => {
+  const variant = {
+    video,
+    source: "http://www.nisor.ru/snews/oa-/iooa-uoo-ooo-a",
+  };
+
+  if (word.endsWith(' 2')) {
+    word.slice(0, -2);
+  }
+
+  const index = _.findIndex(output, {word});
+  if (index == -1) {
+    output.push({word, variants: [variant]});
+  } else {
+    output[index].variants.push(variant);
+  }
+});
+
+console.log(JSON.stringify(output, null, 2));
