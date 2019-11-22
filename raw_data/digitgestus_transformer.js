a8e7806c336a84e1a1a6aa10bcf6b27bb70b7f89
Vitaly Pavlenko <cxielamiko@gmail.com>
2019-11-22 19:25:15 +0300

    More cleanups, digitgestus linking started

--- raw_data/digitgestus_transformer.js
+++ raw_data/digitgestus_transformer.js
@@ -4,20 +4,49 @@ const youtube = require('./digitgestus_youtube.json');
 
 output = [];
 
+// 1. В списке youtube заменить все слова на номера. У вариантов заменять последовательно.
+// 2. Пройтись по input и преобразовать все пары в output.
+
+const wordToNumbers = {};
+
 input.forEach(([word, variants]) => {
-
-
-  const variant = {
-    video,
-    source
-  };
-
-  const index = _.findIndex(output, {word});
-  if (index == -1) {
-    output.push({word, variants: [variant]});
+  if (typeof variants === "string") {
+    wordToNumbers[word] = [variants];
   } else {
-    output[index].variants.push(variant);
+    wordToNumbers[word] = variants.slice();
   }
 });
 
-console.log(JSON.stringify(output, null, 2));
+// console.log(JSON.stringify(wordToNumbers));
+
+const numberToVideo = {};
+
+for (const value of youtube) {
+  if (!new RegExp("[0-9]{4}").test(value.word)) {
+    if (wordToNumbers[value.word] === undefined || wordToNumbers.length === 0) {
+      throw value.word;
+    }
+    numberToVideo[wordToNumbers[value.word].pop()] = value.video;
+  } else {
+    numberToVideo[value.word] = value.video;
+  }
+}
+
+// console.log(JSON.stringify(numberToVideo));
+
+input.forEach(([word, variants]) => {
+  output.push({
+    word,
+    "variants": (typeof variants === "string" ? [variants] : variants).map(number => {
+      if (!numberToVideo[number]) {
+        throw number;
+      }
+      return {
+        "video": numberToVideo[number],
+        "source": "http://www.digitgestus.com/"
+      };
+    })
+  });
+});
+
+// console.log(JSON.stringify(output, null, 2));
