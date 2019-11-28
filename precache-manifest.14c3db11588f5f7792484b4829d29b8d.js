bc46f36b6172193cebd60691673d754a56315a80
Vitaly Pavlenko <cxielamiko@gmail.com>
2019-11-29 01:09:15 +0300

    Updates

---
+++ precache-manifest.14c3db11588f5f7792484b4829d29b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28afd9d15f2727ad2b2c51c5d6b7f6eb",
+    "url": "/rsl/index.html"
+  },
+  {
+    "revision": "db5e99973cfc5aaad7e5",
+    "url": "/rsl/static/css/main.c6154bf8.chunk.css"
+  },
+  {
+    "revision": "24866c58f8dc6f361e2a",
+    "url": "/rsl/static/js/2.6fc55bde.chunk.js"
+  },
+  {
+    "revision": "db5e99973cfc5aaad7e5",
+    "url": "/rsl/static/js/main.430e1fa7.chunk.js"
+  },
+  {
+    "revision": "eac69cfca75ba45740c3",
+    "url": "/rsl/static/js/runtime-main.45d665b6.js"
+  }
+]);
