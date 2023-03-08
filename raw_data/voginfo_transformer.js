7b8b42617845706cdadf2718970ab36a219b7228
Vitaly Pavlenko <cxielamiko@gmail.com>
2023-03-08 10:39:41 +0400

    #5: prefix voginfo with https

--- raw_data/voginfo_transformer.js
+++ raw_data/voginfo_transformer.js
@@ -1,12 +1,11 @@
-const _ = require("lodash");
 const input = require("./voginfo.json");
 
 output = input.map(({ word, cities }) => ({
   word: word.toLowerCase(),
   variants: cities.map(({ video }) => ({
-    video: `http://voginfo.ru/lexicon/assets/video/webm/${video}`,
-    source: "http://voginfo.ru/lexicon/"
-  }))
+    video: `https://voginfo.ru/lexicon/assets/video/webm/${video}`,
+    source: "https://voginfo.ru/lexicon/",
+  })),
 }));
 
 console.log(JSON.stringify(output, null, 2));
